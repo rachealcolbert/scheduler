de40ccacb3b2eb48cd0b0750ac9b0aac1c1e92ef
Racheal Colbert <rachealcolbert16@gmail.com>
2020-10-23 19:48:32 -0700

    block change color

--- Assets/Java/script.js
+++ Assets/Java/script.js
@@ -1,3 +1,19 @@
+// Today's Date
 var NowMoment = moment();
 var eDisplayMoment = document.getElementById('currentDay');
 eDisplayMoment.innerHTML = NowMoment.format("[Today is] dddd, MMMM Do");
+
+// Change box color based on past, preset, and future time 
+var changeBox = function (changeColor) {
+    var time = moment().format("H");
+
+    if (moment().isAfter(time)) {
+        $(changeColor).addClass("future");
+    };
+
+    if (moment().isBefore(time)) {
+        $(changeColor).addClass("past");
+    } else {
+        $(changeColor).addClass("present")
+    };
+};
