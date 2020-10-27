a85fd6e1945cbc067ab1c365149a7dc9fcd450ac
rachealcolbert <70107799+rachealcolbert@users.noreply.github.com>
2020-10-27 10:50:52 -0700

    Update script.js

--- Assets/Java/script.js
+++ Assets/Java/script.js
@@ -4,25 +4,6 @@ var eDisplayMoment = document.getElementById('currentDay');
 eDisplayMoment.innerHTML = NowMoment.format("[Today is] dddd, MMMM Do");
 
 // Change box color based on past, preset, and future time 
-var changeBox = function () {
-    var currentTime = moment();
-    var currentHour = currentTime.hour();
-
-    $(".hour").each(function () {
-
-        var elementHour = '#time'
-
-        if (moment().hour(elementHour)) isAfter(currentHour); {
-            $(this).addClass("future");
-        };
-        if (moment().hour(elementHour)) isBefore(currentHour); {
-            $(this).addClass("past");
-        }
-        if (moment().hours(elementHour)) {
-            $(this).addClass("present")
-        };
-    });
-};
 
 $(".meeting-event").on("click", "p", function () {
     var text = $(this)
